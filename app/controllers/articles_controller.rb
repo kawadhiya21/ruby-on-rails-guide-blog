b948f892e77946811f540903c02e5ed33709353f
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-06-03 17:07:42 +0530

    Deletion and form partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,11 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
 end
